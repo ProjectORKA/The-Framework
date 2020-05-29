1a122a9f5423f07a6321cd7fdd1c85ea7e980655
Ray Redondo <rdrpenguin04@gmail.com>
2020-05-28 20:47:26 -0500

    Fix unused parameters

--- src/Framework.cpp
+++ src/Framework.cpp
@@ -3,8 +3,10 @@
 //
 
 #include <Framework.h>
-
+#include <iostream>
 
 int main(int argc,char** argv){
-
+    std::cout << "Project ORKA v1.0.0" << std::endl;
+    std::cout << "Program name: " << argv[0] << std::endl;
+    std::cout << "Number of parameters: " << argc << std::endl;
 }
